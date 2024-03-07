3134625970054ed5a0d1bd2da9a5e0b91b1f09e6
agriyantosk <agriyantosk@gmail.com>
2024-03-07 23:36:25 +0700

    feat: Smart Contract & Testing Creation

---
+++ contracts/Escrow.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Escrow {
+    address public arbiter;
+    address public beneficiary;
+    address public depositor;
+
+    bool public isApproved;
+
+    constructor(address _arbiter, address _beneficiary) payable {
+        arbiter = _arbiter;
+        beneficiary = _beneficiary;
+        depositor = msg.sender;
+    }
+
+    event Approved(uint);
+
+    function approve() external {
+        require(!isApproved, "This contract can only be approved once!");
+        require(
+            msg.sender == arbiter,
+            "Only the arbiter can approve this transaction!"
+        );
+        uint balance = address(this).balance;
+        (bool sent, ) = payable(beneficiary).call{value: balance}("");
+        require(sent, "Failed to send Ether");
+        emit Approved(balance);
+        isApproved = true;
+    }
+}
